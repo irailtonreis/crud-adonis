83f3059ff72a0723b0b5fb97be29bec665bcadea
irailtonreis <irailton.oliveira91@gmail.com>
2020-05-25 12:53:53 -0300

    feat: adiciona criação de projects e tasks

---
+++ app/Controllers/Http/ProjectController.js
@@ -0,0 +1,77 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Project = use('App/Models/Project')
+/**
+ * Resourceful controller for interacting with projects
+ */
+class ProjectController {
+  /**
+   * Show a list of all projects.
+   * GET projects
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const projects = Project.query().with('user').fetch()
+
+    return projects
+  }
+
+  /**
+   * Create/save a new project.
+   * POST projects
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response, auth }) {
+    const data = request.only(['title', 'description'])
+
+    const project = await Project.create({ ...data, user_id: auth.user.id })
+
+    return project
+  }
+
+  /**
+   * Display a single project.
+   * GET projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update project details.
+   * PUT or PATCH projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a project with id.
+   * DELETE projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = ProjectController
