735233213cf374778aea3304217686975d0881cc
irailtonreis <irailton.oliveira91@gmail.com>
2020-05-26 14:59:58 -0300

    feat: internacionalização de validações

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const Env = use('Env')
-const Youch = use('youch')
+// const Env = use('Env')
+// const Youch = use('youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
 /**
  * This class handles all exceptions thrown during
